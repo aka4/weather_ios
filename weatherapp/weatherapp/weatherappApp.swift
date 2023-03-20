f0c327d0f579a162af395028403bb8356fce40a7
Atakan Özdemir <atakan.oezdemir@apploft.de>
2023-03-20 15:04:49 +0100

    init project

---
+++ weatherapp/weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Atakan Özdemir on 20.03.23.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
