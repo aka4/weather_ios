cf04d66882889d1435a5efaa165874cd82569de9
Atakan Özdemir <atakan.oezdemir@apploft.de>
2023-09-11 16:58:51 +0200

    add creation date, env for preview, sort data

--- Apploft Weather/Apploft Weather/View/PopoverListView.swift
+++ Apploft Weather/Apploft Weather/View/PopoverListView.swift
@@ -9,24 +9,30 @@ import SwiftUI
 
 struct PopoverListView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var citiesData: FetchedResults<City>
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.creationDate, order: .reverse)]) var citiesData: FetchedResults<City>
     
     var body: some View {
         VStack(spacing: 3) {
-            List(citiesData) { city in
-                 Text(city.name ?? "Unknown")
-             }
+            NavigationStack{
+                List{
+                    ForEach(citiesData) { city in
+                        Text(city.name ?? "Unknown")
+                    }
+                }
+            }
             Button("Add") {
                 let cityNames = ["Hamburg", "Berlin", "New York", "Tokyo", "Istanbul"]
                 let chosenCityName = cityNames.randomElement()!
                 let citay = City(context: moc)
                 citay.id = UUID()
                 citay.name = "\(chosenCityName)"
+                citay.creationDate = Date()
                 try? moc.save()
             }
         }
         .presentationCompactAdaptation(.none)
     }
+
 }
 
 struct PopoverListView_Previews: PreviewProvider {
